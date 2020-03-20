a2ed6ac49e37add430c58bac8b61ce9951caa0bc
vpamrit <vishnuamritpydah@gmail.com>
2020-03-20 18:25:02 -0400

    gears grinding

--- load_data.py
+++ load_data.py
@@ -3,175 +3,104 @@ import random
 import re
 import torch
 import torchvision
+import nibabel as nib
+
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
+from collections import namedtuple
 from torchvision import transforms, utils
+from PIL import Image
 
 from os.path import join, isfile
 from os import listdir
 
-#temporary imports
-import visualize
-from visualize import TestVisualizer
+#constants
+TRAIN_DIR = 'img/'
+IMG_PREFIX = 'img'
+LABEL_DIR = 'label/'
+LABEL_PREFIX = 'label'
+EXT = '.nii.gz'
+SPLEEN_VAL = 1
 
-def get_data(labels_file, root_dir, transform=None, mode="relative"):
-    return PhoneDataset(labels_file=labels_file,
-                                   root_dir=root_dir, transform=transform, mode=mode)
+
+def process_image(img_file, normalize=True):
+
+    if not img_file:
+        return None
+
+    img_3d = nib.load(img_file)
+    img = img_3d.get_data()
+
+    if normalize:
+        img = (img - img.min()) / (img.max() - img.min())
+        img = img * 255.0
+
+    img = np.transpose(img, (2,0,1))
+
+    return img
+
+class Img:
+    def __init__(self, img, label , axis, idx):
+        self.img = img
+        self.label = label
+        self.axis = axis
+        self.idx = idx
+        self.complete = False
 
 
-def read_txt(root_dir, labels_file):
-
-    if isfile(root_dir):
-        files = [root_dir.split('/')[-1]]
-    else:
-        files = [f for f in os.listdir(root_dir) if isfile(join(root_dir, f))]
-
-    labels = [ [line.split(' ',1)[0], float(line.split(' ',2)[1]), float(line.split(' ',2)[2])] for line in open(labels_file) if (line.split(' ',1)[0] in files)]
-    labels = sorted(labels, key=lambda tup: int(tup[0].split('.',1)[0]))
-
-    if len(labels) == 0:
-        labels = [ [f.split(' ', 1)[0], float(0.0), float(0.0)] for f in files ]
-
-    return np.array(labels)
-
-class PhoneDataset(Dataset):
-    def __init__(self, labels_file, root_dir, transform=None, mode="relative"):
-        self.labels = read_txt(root_dir, labels_file)
+class SpleenDataset(Dataset):
+    def __init__(self, root_dir, img_range=(1,1), axis=0, transform=None):
         self.root_dir = root_dir
         self.transform = transform
-        self.mode = mode
+        self.img_range = img_range
+        self.cur_sample = Img(None, None, 0, 0)
+        self.cur_sample.complete = True
 
+        #check if there is a labels
+        if self.root_dir[-1] != '/':
+            self.root_dir += '/'
+
+        self.is_labeled = os.path.isdir(self.root_dir + LABEL_DIR)
+
+    #this is actually incorrect
     def __len__(self):
-        return len(self.labels)
+        return (self.img_range[1] - self.img_range[0] + 1)*147
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.root_dir,
-                               self.labels[idx][0])
-        image = io.imread(img_name)
+        img_num = 1  #idx + self.img_range[0]
+
+
+        if self.cur_sample.complete:
+            img_file = os.path.join(self.root_dir, TRAIN_DIR, IMG_PREFIX + str(img_num).zfill(4) + EXT)
+            label_file = os.path.join(self.root_dir, LABEL_DIR, LABEL_PREFIX + str(img_num).zfill(4) + EXT) if self.is_labeled else None
+
+            self.cur_sample = Img(process_image(img_file), process_image(label_file, False), 0, 0)
+
+        #self.cur_sample should be ready
+        img_slice = self.cur_sample.img[self.cur_sample.idx, : , :]
+        img_slice = img_slice.astype('float32')
+
+        img_label = self.cur_sample.label[self.cur_sample.idx, :, :] if self.is_labeled else np.array([])
+        self.cur_sample.idx += 1
+
+        #if we have an image label filter for the spleen
+        if img_label.size != 0:
+            img_label = img_label == SPLEEN_VAL
+
+        img_label = img_label.astype('float32')
+
+        im = Image.fromarray(np.uint8(img_label))
+        im.save('./gen/gen_' + str(idx).zfill(4) + ".png")
 
         # numpy is W x H x C
         # torch is C x H x W
-        image = torch.from_numpy(image.transpose((2, 0, 1)))
-        target = torch.from_numpy(self.labels[idx, 1:3].astype('float').reshape(-1,2).squeeze())
+        #image = torch.from_numpy(image.transpose((2, 0, 1)))
+        #label = torch.from_numpy(self.labels[idx, 1:3].astype('float').reshape(-1,2).squeeze())
 
-        if self.transform:
-            image = self.transform(image)
+        return img_slice, img_label
 
-        if self.mode == "absolute":
-            target[0] *= image.size()[2]
-            target[1] *= image.size()[1]
-
-        return image, target
-
-    def get_labels(self, idx):
-         if torch.is_tensor(idx):
-             idx = idx.tolist()
-
-         img_name = os.path.join(self.root_dir,
-                                self.labels[idx][0])
-         image = io.imread(img_name)
-
-         # numpy is W x H x C
-         # torch is C x H x W
-         image = torch.from_numpy(image.transpose((2, 0, 1)))
-         target = self.labels[idx, 1:3].astype('float').reshape(-1,2)
-
-         return img_name, image, target
-
-    def find(self, tensor, values):
-        return torch.nonzero(tensor[..., None] == values)
-
-
-#creates new images in a new directory
-#creates a corresponding labels file
-class DatasetBuilder(object):
-    def __init__(self, phone_dataset, result_dir, new_labels_file, PILtransforms, generate=1, overwrite=True):
-        self.PILtransforms = PILtransforms
-        self.dataset = phone_dataset
-        self.result_dir = result_dir+'/' if result_dir[-1] != '/' else result_dir
-        self.new_labels_file = new_labels_file
-        self.gen = generate
-        self.overwrite = overwrite
-
-         #ensures the creation of the directory
-        dirs = re.split("/", result_dir)
-        directory = ""
-
-        for i in range(len(dirs)):
-            directory += dirs[i] + "/"
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
-    def generate(self):
-        #create labels file
-
-        if not self.overwrite:
-            status = "a"
-        else:
-            status="w"
-
-        labels_file = open(self.new_labels_file, status)
-
-        #iterate over the dataset and generate the new samples
-        for s_num in range(len(self.dataset)):
-            for i in range(self.gen):
-                img_name, image, label = self.dataset.get_labels(s_num)
-
-                #let the first sample through with no transforms
-                if i != 0:
-                    sample_image, pil_image, new_label = self.exec_PIL_transforms(image, label)
-                else:
-                    sample_image, pil_image, new_label = img_name, torchvision.transforms.functional.to_pil_image(image), label
-
-                file_name = self.result_dir+re.split('[/.]', img_name)[-2]+"_"+str(i)+"."+img_name.split('.')[-1]
-                print(file_name)
-                pil_image.save(file_name)
-
-                #write the value to the labels file
-                labels_file.write((file_name.split('/')[-1]+" {:.4f} "+" {:.4f}"+"\n").format(new_label[0,0].item(), new_label[0,1].item()))
-
-        labels_file.close()
-
-    # label is a numpy array
-    def exec_PIL_transforms(self, image, label, image_name = None):
-        #transform from tensor to PIL image
-        transformed_sample = torchvision.transforms.functional.to_pil_image(image)
-        transformed_label_image = torchvision.transforms.functional.to_pil_image(torch.zeros(image.size(), dtype=torch.uint8))
-        prev_label = label.copy()
-        safety_pixels = 841 #29*29
-
-        ## compute coords that need to be to 1
-        ## label coords to PIL coords
-        coordx = int(image.size()[2]*label[0][0])
-        coordy = int(image.size()[1]*label[0][1])
-
-        #safe box for the phone (only in training set => so it stays in bounds yikes)
-        dist = lambda x,y: pow(pow(x[0]-y[0], 2) + pow(x[1]-y[1],2), 0.5) / 9.9
-
-        for i in range(29):
-            for j in range(29):
-                x = [coordx-14+i, coordy-14+j]
-                y = [coordx, coordy]
-                transformed_label_image.putpixel((coordx-14+i, coordy-14+j), (int(255*(1-dist(x,y))), 0, 0))
-
-        transformed_label_image.putpixel((coordx, coordy), (255, 255, 255));
-        transforms = self.PILtransforms
-
-        if self.PILtransforms != None:
-            if(random.randint(0,1000) > 500):
-                transforms.reverse()
-            for tsfrm in transforms:
-                transformed_sample, transformed_label_image, prev_label, safety_pixels = tsfrm(transformed_sample, transformed_label_image, prev_label, safety_pixels)
-
-        #transform it
-        label_image = torchvision.transforms.functional.to_tensor(transformed_label_image)
-        sample_image = torchvision.transforms.functional.to_tensor(transformed_sample)
-
-        new_label = torch.from_numpy(prev_label)
-
-        return sample_image, transformed_sample, new_label
