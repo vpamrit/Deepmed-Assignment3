9f88db530c8331146029c4a5bc291a03e6a059bd
vpamrit <vishnuamritpydah@gmail.com>
2020-03-22 18:01:11 -0400

    -

--- plain_dice.py
+++ plain_dice.py
@@ -3,5 +3,5 @@ import torch
 def dice_loss(pred,target):
     numerator = 2 * torch.sum(pred * target)
     denominator = torch.sum(pred + target)
-    return 1 - (numerator + 1) / (denominator + 1)
+    return (numerator + 1) / (denominator + 1) #-1
 
