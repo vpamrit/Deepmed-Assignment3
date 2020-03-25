11aebad90765b6e72c1ef83f1315716d01712738
vpamrit <vishnuamritpydah@gmail.com>
2020-03-25 13:44:19 -0400

    -

--- main_unet.py
+++ main_unet.py
@@ -70,7 +70,7 @@ args = parser.parse_args()
 args.cuda = args.cuda and torch.cuda.is_available()
 
 DATA_FOLDER = args.data_folder
-THRESHOLD = 0.005
+THRESHOLD = 0.006
 
 # %% Loading in the Dataset
 dset_train = SpleenDataset(DATA_FOLDER, tuple(args.train_img_range), SLICE_SIZE, 150, 3, classes=CLASSES, threshold=THRESHOLD)
@@ -229,8 +229,8 @@ for i in tqdm(range(args.epochs)):
     train(i, loss_list, counter)
     torch.save(model.state_dict(), SAVE_DIR + 'deeplabv3-final-{}.pth'.format(i))
 
-    if (i+1) % 8 == 0:
-        THRESHOLD = THRESHOLD / 2 if THRESHOLD > 0.003 else 0.00
+    if (i+1) % 7 == 0:
+        THRESHOLD = THRESHOLD / 2 if THRESHOLD >= 0.003 else 0.00
         dset_train.clean(THRESHOLD)
         dset_valid.clean(THRESHOLD)
 
